abd757f6df49aff54121c4ec69d590344eb00e7f
Philip <philipmjobe@gmail.com>
2021-11-02 13:41:03 -0400

    added eslint and prettier

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const chart = Highcharts.chart('container', {
-      chart: {
-          type: 'bar'
-      },
+document.addEventListener("DOMContentLoaded", function () {
+  const chart = Highcharts.chart("container", {
+    chart: {
+      type: "bar",
+    },
+    title: {
+      text: "Fruit Consumption",
+    },
+    xAxis: {
+      categories: ["Apples", "Bananas", "Oranges"],
+    },
+    yAxis: {
       title: {
-          text: 'Fruit Consumption'
+        text: "Fruit eaten",
       },
-      xAxis: {
-          categories: ['Apples', 'Bananas', 'Oranges']
+    },
+    series: [
+      {
+        name: "Jane",
+        data: [1, 10, 4],
       },
-      yAxis: {
-          title: {
-              text: 'Fruit eaten'
-          }
+      {
+        name: "John",
+        data: [5, 7, 3],
       },
-      series: [{
-          name: 'Jane',
-          data: [1, 0, 4]
-      }, {
-          name: 'John',
-          data: [5, 7, 3]
-      }]
+    ],
   });
 });
